b4a41f71d7f687af8a6b7cead75f6b56aa3337ee
othmantitee <othman22022@gmail.com>
2019-02-17 16:35:51 +0200

    Great GroupedProduct Module

---
+++ app/code/CMagento/GroupedProduct/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: othman
+ * Date: 2/17/19
+ * Time: 4:23 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CMagento_GroupedProduct',
+    __DIR__
+);
